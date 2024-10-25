481d27a311febbab8a1c09ade4abf8919ddc263e
VIJI C <vijichinnaraj2001@gmail.com>
2024-10-25 15:00:26 +0530

    Optimized soulltion for Sort Colors.java

--- Arrays/Sort Colors.java
+++ Arrays/Sort Colors.java
@@ -62,3 +62,35 @@ class Solution {
         }
     }
 }
+
+// Optimal Approach
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int len = nums.length;
+        int high = len-1;
+        int low = 0, mid = 0;
+        while(mid<=high)
+        {
+            if(nums[mid] == 0)
+            {
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+}
