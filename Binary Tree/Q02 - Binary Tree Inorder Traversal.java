a4c3e17b7db3981424687ff26f099db97ebfa30e
VIJI C <vijichinnaraj2001@gmail.com>
2025-09-21 12:55:02 +0530

    Binary Tree Inorder Traversal

--- Binary Tree/Q02 - Binary Tree Inorder Traversal.java
+++ Binary Tree/Q02 - Binary Tree Inorder Traversal.java
@@ -58,3 +58,84 @@ class Solution {
         return list;
     }
 }
+
+// Iterative Approach
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+        // inorder(root, result);
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while(!stack.isEmpty() || curr!=null)
+        {
+            while(curr != null)
+            {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            curr = node.right;
+        }
+        return result;
+    }
+}
+
+// Solution 2
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+        // inorder(root, result);
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while(!stack.isEmpty() || curr!=null)
+        {
+            if(curr != null)
+            {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            else
+            {
+                TreeNode node = stack.pop();
+                result.add(node.val);
+                curr = node.right;
+            }
+        }
+        return result;
+    }
+}
